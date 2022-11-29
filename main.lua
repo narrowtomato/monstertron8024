6fb40b4bc388f8ef9d5ece866f50ea668bdd6fff
narrowtomato <jester.fett@gmail.com>
2022-11-28 18:34:57 -0600

    hazards

--- main.lua
+++ main.lua
@@ -12,6 +12,8 @@ function love.load()
     gameState = 2
 
     things = {}
+
+    populateStage(20)
 end
 
 function love.update(dt)
@@ -21,13 +23,34 @@ end
 function love.draw()
     -- Debug info
     love.graphics.setColor(1, 1, 1)
+    love.graphics.print(player.x .. " " .. player.y)
 
     -- Draw player
     player:draw()
 
     -- Draw Things
-    for i,t in ipairs(things) do 
-        
+    for k,t in ipairs(things) do 
+        love.graphics.setColor(t.color[1], t.color[2], t.color[3])
+        love.graphics.circle("fill", t.x, t.y, t.radius)
     end
 end
 
+function populateStage(num_hazards)
+    -- Create hazards
+    for i = num_hazards, 1, -1 do
+        local temp_x = player.x
+        local temp_y = player.y
+        -- Precalculate these and make sure they're not on top of the player 
+        while math.abs(temp_x - player.x) < (player.radius * 4) and math.abs(temp_y - player.y) < (player.radius * 4) do
+            temp_x = love.math.random(0, love.graphics.getWidth())
+            temp_y = love.math.random(0, love.graphics.getHeight())
+        end
+        local hazard = {
+            color = {1, 0, 0},
+            x = temp_x,
+            y = temp_y,
+            radius = love.math.random(10, 20) 
+        }
+        table.insert(things, hazard)
+    end
+end
